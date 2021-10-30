0955599c24f49ee3cb23b81c218166f4cd5ecda0
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2021-10-29 21:14:39 -0600

    finished midterm project!
    finished my tool for web midterm project. there are no errors but there is one warning on a couple of the pages. Its just about not having a header in my section. <3

--- Midterm/index.js
+++ Midterm/index.js
@@ -1,13 +1,66 @@
-  
 
-const images = ["babyrocket.jpg", "cover1.jpeg", "cover2.jpg", "roundstar.jpg"]
 
-for (i = 0; i < document.getElementsByClassName("picture").length; i ++){
-    console.log(document.getElementsByClassName("label")[i].innerHTML); 
-    document.getElementsByClassName("picture")[i].innerHTML += "<img src= ' " +  images[i] + " ' > "; 
-    
+
+
+// PICTURE PAGE 
+//this is javascript to iterate through every flex box and populate it with every image in this list.
+
+const images = ["cover1.jpeg", "babyrocket.jpg", "cover2.jpg", "cover3.jpg", "stamp1.jpg", "stamp2.jpg", "stamp3.jpg", "stamp4.jpg"]
+
+for (i = 0; i < document.getElementsByClassName("picture").length; i++) {
+    console.log(document.getElementsByClassName("label")[i].innerHTML);
+    document.getElementsByClassName("picture")[i].innerHTML += "<img src= ' " + images[i] + " ' > ";
     //have to put the image relative to the pictures.html page, not the js page 
 }
 
-console.log("do you see this? ")
 
+document.getElementById("icon").addEventListener("click", randomBG); 
+
+function randomBG() {
+    document.body.style.backgroundColor = `rgba(` + Math.random() * 255 + `, ` + Math.random() * 255 + `, ` + Math.random() * 255 + `, 255)`
+}
+// setInterval(randomBG, 100)
+
+
+// document.getElementById("button").addEventListener("click", console.log(Date())); 
+
+// // buttonClicked(){
+// //     document.getElementById("button").addEventListener("click", Date())
+// // }
+
+
+
+
+
+
+//CRAFTS PAGE 
+
+
+const craftimages = ["crochet1.jpg", "crochet2.jpg", "crochet3.jpg", "crochet4.jpg"]
+
+for (i = 0; i < document.getElementsByClassName("craft").length; i++) {
+    console.log(document.getElementsByClassName("label2")[i].innerHTML);
+    document.getElementsByClassName("craft")[i].innerHTML += "<img src= ' " + craftimages[i] + " ' > ";
+}
+
+
+
+
+
+//READING PAGE 
+const readingimages = ["book1.jpg", "book2.jpg", "book3.jpg", "book4.jpg"]
+
+for (i = 0; i < document.getElementsByClassName("reading").length; i++) {
+    console.log(document.getElementsByClassName("label3")[i].innerHTML);
+    document.getElementsByClassName("reading")[i].innerHTML += "<img src= ' " + readingimages[i] + " ' > ";
+}
+
+
+
+//RECIPES PAGE 
+const recipeimages = ["recipe1.jpg", "recipe2.jpg", "recipe3.jpg", "recipe4.jpg"]
+
+for (i = 0; i < document.getElementsByClassName("recipe").length; i++) {
+    console.log(document.getElementsByClassName("label4")[i].innerHTML);
+    document.getElementsByClassName("recipe")[i].innerHTML += "<img src= ' " + recipeimages[i] + " ' > ";
+}
