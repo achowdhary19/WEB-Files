91755ea3ea82fc582d1960cd58a7f56909e18a97
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2021-12-11 13:57:02 -0700

    final web project: iSPY
    most of the html and styling  is done, just need to figure out how to fix image size and the hovering. I might add a couple more images.

---
+++ Final/index.js
@@ -0,0 +1,10 @@
+
+
+
+document.getElementById("chew").addEventListener("click", myFunction);
+
+// Why does it default to getElements? And why wont it work to use the class id? ok i got it to work with class id but why do i need a number. maybe i should change it to image id so that the lcicking is specific 
+
+function myFunction() {
+  alert ("Hello World!");
+}
--- Midterm/index.js
+++ Midterm/index.js
@@ -1,14 +1,12 @@
-
-
-
-
 // PICTURE PAGE 
 //this is javascript to iterate through every flex box and populate it with every image in this list.
 
 const images = ["cover1.jpeg", "babyrocket.jpg", "cover2.jpg", "cover3.jpg", "stamp1.jpg", "stamp2.jpg", "stamp3.jpg", "stamp4.jpg"]
 
 for (i = 0; i < document.getElementsByClassName("picture").length; i++) {
-    console.log(document.getElementsByClassName("label")[i].innerHTML);
+    
+    console.log("length: " + document.getElementsByClassName("picture").length)
+    // console.log(document.getElementsByClassName("label")[i].innerHTML);
     document.getElementsByClassName("picture")[i].innerHTML += "<img src= ' " + images[i] + " ' > ";
     //have to put the image relative to the pictures.html page, not the js page 
 }
@@ -19,6 +17,8 @@ document.getElementById("icon").addEventListener("click", randomBG);
 function randomBG() {
     document.body.style.backgroundColor = `rgba(` + Math.random() * 255 + `, ` + Math.random() * 255 + `, ` + Math.random() * 255 + `, 255)`
 }
+
+
 // setInterval(randomBG, 100)
 
 
