5ee41f5f4096a40f00f1ab6fb302cfde8fdafd0f
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2022-01-01 21:38:34 -0600

    hoink

--- For fun/index.js
+++ For fun/index.js
@@ -3,16 +3,16 @@
 
 let sound1 = new Audio("assets/Conga.wav");
 document.getElementById("spotify").addEventListener("click",function(){
-  sound1.play();
+  sound2.play();
 })
 
 document.getElementById("applem").addEventListener("click",function(){
-    sound1.play();
+    sound2.play();
   })
   
 
 document.getElementById("soundcloud").addEventListener("click",function(){
-sound1.play();
+sound2.play();
 })
 
 
