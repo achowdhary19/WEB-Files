84374f2a9c8d35ae29b9108ce13cc70f7ab14ead
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2022-01-01 21:10:19 -0600

    made link grid smaller

--- For fun/index.js
+++ For fun/index.js
@@ -18,3 +18,19 @@ sound1.play();
 
 let sound2 = new Audio("assets/HouseHit.wav");
 
+document.getElementById("album").addEventListener("click",function(){
+  sound2.play();
+  })
+
+document.getElementById("album2").addEventListener("click",function(){
+  sound2.play();
+  })
+    
+
+document.getElementById("album3").addEventListener("click",function(){
+  sound2.play();
+  })
+
+document.getElementById("album4").addEventListener("click",function(){
+  sound2.play();
+  })
