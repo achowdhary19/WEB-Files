a444c718250914ee9c439ebe9a2e3652fc5bd388
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2022-01-11 14:06:59 -0600

    blur

--- Final/index.js
+++ Final/index.js
@@ -208,7 +208,7 @@ function changePic(){
   if (image_tracker == "journal"){
     image.src= "assets/openjournal.png";
     image_tracker = "open";
-    image.style.filter =  "blur(.20px)";
+    image.style.filter =  "blur(.13px)";
   }
   else if (image_tracker == "open"){
     image.src = "assets/journal.png";
