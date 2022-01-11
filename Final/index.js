129ecff8ad93842d3baf9e272ec8b3ad3caa763f
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2022-01-11 11:59:08 -0600

    trying to fix background image for mobile

--- Final/index.js
+++ Final/index.js
@@ -1,11 +1,27 @@
 
 
 // ALERT 
-document.getElementById("chew").addEventListener("click", helloWorld);
+const lines = ["There was an old lady who swallowed a cow. ", "She swallowed the cow to catch the goat... ", "She swallowed the goat to catch the dog... ", "She swallowed the dog to catch the cat... ", "She swallowed the cat to catch the bird ... ", "She swallowed the bird to catch the spider ", "That wiggled and wiggled and tickled inside her. ", "She swallowed the spider to catch the fly. ", "She swallowed the spider to catch the fly. ", "But I dunno why she swallowed that fly ", "Perhaps she'll die. ", "There was an old lady who swallowed a horse - ", "She's dead, of course. "] 
+let lineCount = 0;
+document.getElementById("chew").addEventListener("click", function(){ 
 
-function helloWorld() {
-  alert ("Arfas little world");
-}
+  if (lineCount >= lines.length){
+    console.log(lineCount);
+    document.getElementById("story").innerHTML += lines[lines.length-1]
+    lineCount ++;
+  }
+
+  else if (lineCount == 24){
+    alert("Shes dead");
+  }
+  else {
+    document.getElementById("story").innerHTML += lines[lineCount]
+    console.log(lineCount);
+    lineCount ++;
+  }
+
+});
+
 
 
 // ROTATE HAND 
@@ -15,6 +31,19 @@ function animate(){
       document.getElementById("hand").style.WebkitTransitionDuration=".25s";
       document.getElementById("hand").style.transform = 'rotate(240deg)'; } , 1000)
 }
+
+// //animate crochet hook 
+// document.getElementById("hook").addEventListener("click", moveHook);
+
+// function moveHook(){
+//   setInterval(function(){
+//     document.getElementById("hook").style.WebkitTransitionDuration=".10s";
+//     document.getElementById("hook").style.transform = 'translateX(-80px) rotate(-40deg) translateY(80px)' ; 
+
+//   } , 1000)
+
+// }
+  
 
 
 
@@ -83,10 +112,16 @@ document.getElementById("flower").addEventListener("click",function(){
 
 // CROCHET BOY
 let n = 0;
+let yay = new Audio("assets/yay.m4a");
 document.getElementById("crochetboy").addEventListener("click", function(){
-  document.body.style.backgroundColor = "black";
-  document.querySelectorAll("img")[n].src = "assets/crochetBoy.png";
-  n++; 
+
+    document.body.style.backgroundColor = "black";
+    document.querySelectorAll("img")[n].src = "assets/crochetBoy.png";
+    n++; 
+    if (n == 27){
+      yay.play();
+    }
+    
 });
 
 
@@ -127,6 +162,29 @@ document.getElementById("yellow1").addEventListener("click", function(){
   b.play();
 })
 
+document.getElementById("orange2").addEventListener("click", function(){
+  c.play();
+})
+
+document.getElementById("pink2").addEventListener("click", function(){
+  d.play();
+})
+
+document.getElementsByClassName("item11-1")[0].addEventListener("click", function(){
+  e.play();
+})
+
+document.getElementsByClassName("item11-2")[0].addEventListener("click", function(){
+  f.play();
+})
+
+document.getElementsByClassName("item11-3")[0].addEventListener("click", function(){
+  g.play();
+})
+
+document.getElementsByClassName("item11-4")[0].addEventListener("click", function(){
+  d.play();
+})
 
 
 
@@ -161,7 +219,7 @@ function changePic(){
 
 
 //LIGHTS OUT 
-let sound4 = new Audio("assets/lightSwitchCrop.mov");
+let sound4 = new Audio("assets/lightSwitchCROP.m4a");
 document.getElementById("circle").addEventListener("click", lightsOut);
 
 var lightsOn = true; 
@@ -186,5 +244,20 @@ document.getElementById("mushroom").addEventListener("click", function(){
   document.body.style.backgroundImage = "url('assets/field.png')";
   document.body.style.backgroundPosition = "center";
   birds.play();
-
 })
+
+
+//blur page 
+const items =  document.querySelectorAll("img");
+document.getElementsByClassName("item19")[0].addEventListener("click", function(){
+  for (var i = 0, l = items.length; i < l; i++){
+  document.querySelectorAll("img")[i].style = "filter: blur(10px)";
+  document.getElementsByTagName("h1")[0].style = "filter: blur(4px)";
+  document.getElementById("sharpen").style = "filter: blur(13px)";
+  document.getElementsByTagName("button")[0].style = "filter: blur(5px)";
+  document.getElementsByTagName("p")[0].style = "filter: blur(5px)";
+  }
+})
+
+
+
